4f5e620c9da04ece8f46cf61b05ad8ec15761c8d
Niklas Bunge <5bunge@uni-hamburg.de>
2015-11-19 17:24:26 +0100

    plan2 zu 3 geändert

--- scripts/move_group_plan3.py
+++ scripts/move_group_plan3.py
@@ -1,43 +1,6 @@
 #!/usr/bin/env python
 
-# Software License Agreement (BSD License)
-#
-# Copyright (c) 2013, SRI International
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-#  * Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-#  * Redistributions in binary form must reproduce the above
-#    copyright notice, this list of conditions and the following
-#    disclaimer in the documentation and/or other materials provided
-#    with the distribution.
-#  * Neither the name of SRI International nor the names of its
-#    contributors may be used to endorse or promote products derived
-#    from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#
-# Author: Acorn Pooley
 
-## BEGIN_SUB_TUTORIAL imports
-##
-## To use the python interface to move_group, import the moveit_commander
-## module.  We also import rospy and some messages that we will use.
 import sys
 import copy
 import rospy
@@ -116,8 +79,8 @@ def move_group_python_interface_tutorial():
   
   
   
-  print "planning plan2"
-  plan1 = group.plan()
+  print "planning plan3"
+  plan3 = group.plan()
   
   
   
@@ -126,15 +89,15 @@ def move_group_python_interface_tutorial():
   display_trajectory = moveit_msgs.msg.DisplayTrajectory()
 
   display_trajectory.trajectory_start = robot.get_current_state()
-  display_trajectory.trajectory.append(plan2)
+  display_trajectory.trajectory.append(plan3)
   display_trajectory_publisher.publish(display_trajectory);
   
   
   
   
   
-  print "execute plan2"
-  group.execute(plan2)
+  print "execute plan3"
+  group.execute(plan3)
   print "plan2 finished"
   
   

91e211c73f94e6ed2a3b6d410bbe7409e93d4063
15bunge <5bunge@uni-hamburg.de>
2016-01-18 12:24:43 -0800

    anpassung fuer echten arm

--- src/cppGreifeTisch_legeBoden.cpp
+++ src/cppGreifeTisch_legeBoden.cpp
@@ -18,65 +18,148 @@ int main(int argc, char **argv)
   ros::AsyncSpinner spinner(1);
   spinner.start();
 
-
-  moveit::planning_interface::MoveGroup group("manipulator");
-
   moveit::planning_interface::PlanningSceneInterface planning_scene_interface;  
+  moveit::planning_interface::MoveGroup group("UR5_arm");
+
+  
+//transform koordinaden abgreifen
+  double posx = 0.5;
+  double posy = 0.3;
+  
 
   
   group.setPlannerId("RRTConnectkConfigDefault");
-  group.setPlanningTime(240);
+  group.setPlanningTime(120);
+  group.setNumPlanningAttempts(50);
   
   group.clearPoseTargets();
   group.clearPathConstraints();
   
-  geometry_msgs::Pose pose_target;
-  pose_target.orientation.x = 0.5;
-  pose_target.orientation.y = 0.5;
-  pose_target.orientation.z = -0.5;
-  pose_target.orientation.w = 0.5;
-  pose_target.position.x = 0.7;
-  pose_target.position.y = 0.4;
-  pose_target.position.z = 1.2;
   
-  group.setPoseTarget(pose_target);
+  geometry_msgs::Pose pose_start;
+  pose_start.orientation.w = 1;
+  pose_start.position.x = 0.8;
+  pose_start.position.y = 0.8;
+  pose_start.position.z = 1.25;
+    
+  geometry_msgs::Pose pose_grab;
+  pose_grab.orientation.x = 0.5;
+  pose_grab.orientation.y = 0.5;
+  pose_grab.orientation.z = -0.5;
+  pose_grab.orientation.w = 0.5;
+  pose_grab.position.x = posx;
+  pose_grab.position.y = posy;
+  pose_grab.position.z = 1.2;
+  
+  geometry_msgs::Pose pose_place;
+  pose_place.orientation.x = 0.5;
+  pose_place.orientation.y = 0.5;
+  pose_place.orientation.z = -0.5;
+  pose_place.orientation.w = 0.5;
+  pose_place.position.x = 0.35;
+  pose_place.position.y = 1.35;
+  pose_place.position.z = 0.8;
+  
+  
+  geometry_msgs::Pose pose_bottle;
+  pose_bottle.orientation.w = 1;
+  pose_bottle.position.x = posx;
+  pose_bottle.position.y = posy;
+  pose_bottle.position.z = 0.885;
+  
+  
+  moveit_msgs::CollisionObject collision_object;
+  collision_object.header.frame_id = group.getPlanningFrame();
+  collision_object.id = "flasche";
 
+  shape_msgs::SolidPrimitive primitive;
+  primitive.type = primitive.BOX;
+  primitive.dimensions.resize(3);
+  primitive.dimensions[0] = 0.08;
+  primitive.dimensions[1] = 0.08;
+  primitive.dimensions[2] = 0.28;
 
-  moveit::planning_interface::MoveGroup::Plan my_plan;
-  bool success = group.plan(my_plan);
+  collision_object.primitives.push_back(primitive);
+  collision_object.primitive_poses.push_back(pose_bottle);
+  collision_object.operation = collision_object.ADD;
 
-  ROS_INFO("Visualizing plan 1 (pose goal) %s",success?"":"FAILED");    
-  success = group.execute(my_plan);
-  ROS_INFO("Execution plan 1 (pose goal) %s",success?"":"FAILED");
-  sleep(1.0);
+  std::vector<moveit_msgs::CollisionObject> collision_objects;
+  collision_objects.push_back(collision_object);
 
-  group.attachObject("flasche3");
+  std::vector<std::string> object_ids;
+  object_ids.push_back(collision_object.id);
+  
+  
+  ROS_INFO("bewege zu startzustand");
+  group.setPoseTarget(pose_start);
+  bool success = group.move();
+  if(!success) {
+    ROS_INFO("FAILED SHUTTING DOWN");
+    planning_scene_interface.removeCollisionObjects(object_ids);
+    ros::shutdown();  
+    return 0;
+  }
+  ROS_INFO("OK");    
   sleep(2.0);
 
-  robot_state::RobotState start_state(*group.getCurrentState());
-  geometry_msgs::Pose start_pose2;
-  start_pose2.orientation = pose_target.orientation;
-  start_pose2.position.x = 0.7;
-  start_pose2.position.y = 1.0;
-  start_pose2.position.z = 0.6;
-
-
-  group.setPoseTarget(start_pose2);
-  success = group.plan(my_plan);
-  sleep(10.0);
-  ROS_INFO("Visualizing plan 3 (constraints) %s",success?"":"FAILED");
-  success = group.execute(my_plan);
-  ROS_INFO("Execution plan 3 (constraints) %s",success?"":"FAILED");
-
-
-  // When done with the path constraint be sure to clear it.
-  group.clearPathConstraints();
-  group.clearPoseTargets();
+  ROS_INFO("fuege Flasche ein");
+  planning_scene_interface.addCollisionObjects(collision_objects);
+  ROS_INFO("OK");
   
-  group.detachObject("flasche3");
-  sleep(2);
+  ROS_INFO("bewege zu greifposition");    
+  group.setPoseTarget(pose_grab);
+  success = group.move();
+  if(!success) {
+    ROS_INFO("FAILED SHUTTING DOWN");
+    planning_scene_interface.removeCollisionObjects(object_ids);
+    ros::shutdown();  
+    return 0;
+  }
+  ROS_INFO("OK");    
+  sleep(2.0);
+
+  ROS_INFO("Flasche anhaengen");    
+  group.attachObject(collision_object.id);
+  ROS_INFO("OK");    
+
+  
+  ROS_INFO("bewege zu turtle");    
+  group.setPoseTarget(pose_place);
+  success = group.move();
+  if(!success) {
+    ROS_INFO("FAILED SHUTTING DOWN");
+    planning_scene_interface.removeCollisionObjects(object_ids);
+    ros::shutdown();  
+    return 0;
+  }
+  ROS_INFO("OK");    
+  sleep(2.0);
+  
+  ROS_INFO("Flasche abhaengen");    
+  group.detachObject(collision_object.id);
+  ROS_INFO("OK"); 
+  sleep(2.0);
+  
+  ROS_INFO("bewege zu endzustand");    
+  group.setPoseTarget(pose_start);
+  success = group.move();
+  if(!success) {
+    ROS_INFO("FAILED SHUTTING DOWN");
+    planning_scene_interface.removeCollisionObjects(object_ids);
+    sleep(2.0);
+    ros::shutdown();  
+    return 0;
+  }
+  sleep(2.0);
+
+  ROS_INFO("entferne objekte");
+  planning_scene_interface.removeCollisionObjects(object_ids);
+  sleep(2.0);
+
+  
+  ROS_INFO("ENDE");
+
 
   ros::shutdown();  
   return 0;
 }
-

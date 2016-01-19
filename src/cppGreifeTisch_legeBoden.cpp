cf1b344bb2e089eb3926ddd50eaee9c10dc82692
15bunge <5bunge@uni-hamburg.de>
2016-01-19 01:17:05 -0800

    handansteuerung

--- src/cppGreifeTisch_legeBoden.cpp
+++ src/cppGreifeTisch_legeBoden.cpp
@@ -11,9 +11,12 @@
 #include <ompl/base/StateValidityChecker.h>
 #include <ompl/control/StatePropagator.h>
 
+#include <robotiq_s_model_control/SModel_robot_output.h>
+
+
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "move_group_interface_tutorial");
+  ros::init(argc, argv, "armroutine");
   ros::NodeHandle node_handle;  
   ros::AsyncSpinner spinner(1);
   spinner.start();
@@ -26,10 +29,19 @@ int main(int argc, char **argv)
   double posx = 0.5;
   double posy = 0.3;
   
-
+  
+ //######### hand setup
+  ros::NodeHandle n;
+  ros::Publisher hand_pub = n.advertise<robotiq_s_model_control::SModel_robot_output>("SModelRobotOutput", 1000);
+  robotiq_s_model_control::SModel_robot_output close_command;
+  close_command.rPRA = 255;
+  
+  robotiq_s_model_control::SModel_robot_output open_command;
+  open_command.rPRA = 0;
+  
   
   group.setPlannerId("RRTConnectkConfigDefault");
-  group.setPlanningTime(120);
+  group.setPlanningTime(240);
   group.setNumPlanningAttempts(50);
   
   group.clearPoseTargets();
@@ -37,10 +49,13 @@ int main(int argc, char **argv)
   
   
   geometry_msgs::Pose pose_start;
-  pose_start.orientation.w = 1;
-  pose_start.position.x = 0.8;
-  pose_start.position.y = 0.8;
-  pose_start.position.z = 1.25;
+  pose_start.orientation.x = 0.5;
+  pose_start.orientation.y = 0.5;
+  pose_start.orientation.z = -0.5;
+  pose_start.orientation.w = 0.5;
+  pose_start.position.x = 0.74;
+  pose_start.position.y = 0.56;
+  pose_start.position.z = 1.2;
     
   geometry_msgs::Pose pose_grab;
   pose_grab.orientation.x = 0.5;
@@ -101,6 +116,10 @@ int main(int argc, char **argv)
   }
   ROS_INFO("OK");    
   sleep(2.0);
+  
+  ROS_INFO("oeffne hand");    
+  hand_pub.publish(open_command);
+  sleep(5.0);
 
   ROS_INFO("fuege Flasche ein");
   planning_scene_interface.addCollisionObjects(collision_objects);
@@ -120,8 +139,12 @@ int main(int argc, char **argv)
 
   ROS_INFO("Flasche anhaengen");    
   group.attachObject(collision_object.id);
-  ROS_INFO("OK");    
-
+  ROS_INFO("OK");  
+  sleep(2.0);
+  
+  ROS_INFO("Schließe hand");    
+  hand_pub.publish(close_command);  
+  sleep(5.0);
   
   ROS_INFO("bewege zu turtle");    
   group.setPoseTarget(pose_place);
@@ -139,6 +162,10 @@ int main(int argc, char **argv)
   group.detachObject(collision_object.id);
   ROS_INFO("OK"); 
   sleep(2.0);
+  
+  ROS_INFO("oeffne hand");    
+  hand_pub.publish(open_command);
+  sleep(5.0);
   
   ROS_INFO("bewege zu endzustand");    
   group.setPoseTarget(pose_start);
